a018a696998ddb039cc7eb27f268d51a362c7714
qiaozp <47812250+chivalryq@users.noreply.github.com>
2023-01-03 15:14:08 +0800

    Feat: transform event to metrics (#251)
     transform event statistic to prometheus metrics

---
+++ metrics/prometheus/judge.go
@@ -0,0 +1,71 @@
+package prometheus
+
+import (
+	v1 "k8s.io/api/core/v1"
+	"strings"
+)
+
+// always can be used when no finer-grained reason is reached. It should be the last one when used.
+func always(event *v1.Event) bool {
+	return true
+}
+
+func isPodImagePullBackOff(event *v1.Event) bool {
+	if event.Reason == "Failed" {
+		return strings.Contains(event.Message, "ImagePullBackOff") || strings.Contains(event.Message, "ErrImagePull")
+	}
+	if event.Reason == "BackOff" {
+		return strings.Contains(event.Message, "Back-off pulling image")
+	}
+	return false
+}
+
+func isFailCreatePodExceedQuota(event *v1.Event) bool {
+	return event.Reason == "FailedCreate" && strings.Contains(event.Message, "exceeded quota")
+}
+
+func isFailCreateContainerDiskNotEnough(event *v1.Event) bool {
+	return event.Reason == "FailedCreatePodSandBox" && strings.Contains(event.Message, "no space left on device")
+}
+
+func isResourceInsufficient(event *v1.Event) bool {
+	return event.Reason == "FailedScheduling" && strings.Contains(event.Message, "Insufficient")
+}
+
+func isPodFailStart(event *v1.Event) bool {
+	return event.Reason == "Failed" &&
+		event.InvolvedObject.Kind == "Pod" &&
+		!strings.Contains(event.Message, "ImagePullBackOff") &&
+		!strings.Contains(event.Message, "Failed to pull image")
+}
+
+func isPodCrash(event *v1.Event) bool {
+	return event.Reason == "BackOff" &&
+		strings.Contains(event.Message, "Back-off restarting failed container")
+}
+
+func isDiskProvisionFailSize(event *v1.Event) bool {
+	return event.Reason == "ProvisioningFailed" &&
+		strings.Contains(event.Message, "disk size is not supported")
+}
+
+func isDiskProvisionFail(event *v1.Event) bool {
+	return event.Reason == "ProvisioningFailed" &&
+		!strings.Contains(event.Message, "disk size is not supported")
+}
+
+func isFailedBindingNoStorageClass(event *v1.Event) bool {
+	return event.Reason == "FailedBinding" && strings.Contains(event.Message, "no storage class is set")
+}
+
+func isNodePLEGUnhealthy(event *v1.Event) bool {
+	return event.Reason == "NodeNotReady" && event.InvolvedObject.Kind == "Node" && strings.Contains(event.Message, "PLEG is not healthy")
+}
+
+func isNodeNotReady(event *v1.Event) bool {
+	return event.Reason == "NodeNotReady" && event.InvolvedObject.Kind == "Node" && !strings.Contains(event.Message, "PLEG is not healthy")
+}
+
+func isClusterIPNotEnough(event *v1.Event) bool {
+	return strings.Contains(event.Message, "IpNotEnough")
+}
---
+++ metrics/prometheus/metrics.go
@@ -0,0 +1,249 @@
+package prometheus
+
+import (
+	"github.com/prometheus/client_golang/prometheus"
+	v1 "k8s.io/api/core/v1"
+)
+
+type AbnormalEventReason string
+
+var (
+	// Namespace Lavel Event
+	PodEvict                         AbnormalEventReason = "PodEvict"
+	PodImagePullBackOff              AbnormalEventReason = "PodImagePullBackOff"
+	PodOOM                           AbnormalEventReason = "PodOOM"
+	ResourceInsufficient             AbnormalEventReason = "ResourceInsufficient"
+	PodFailStart                     AbnormalEventReason = "PodFailStart"
+	PodCrash                         AbnormalEventReason = "PodCrash"
+	PodFailScheduling                AbnormalEventReason = "PodFailScheduling"
+	DiskProvisionFailSize            AbnormalEventReason = "DiskProvisionFailSize"
+	DiskProvisionFail                AbnormalEventReason = "DiskProvisionFail"
+	FailedBindingNoStorageClass      AbnormalEventReason = "FailedBindingNoStorageClass"
+	VolumeFailMount                  AbnormalEventReason = "VolumeFailMount"
+	FailCreatePodExceedQuota         AbnormalEventReason = "FailCreatePodExceedQuota"
+	FailCreateContainerDiskNotEnough AbnormalEventReason = "FailCreateContainerDiskNotEnough"
+
+	// Node Level Event
+	NodeOOM           AbnormalEventReason = "NodeOOM"
+	NodeRebooted      AbnormalEventReason = "NodeRebooted"
+	NodeDiskPressure  AbnormalEventReason = "NodeDiskPressure"
+	NodeDockerHung    AbnormalEventReason = "NodeDockerHung"
+	NodePSHung        AbnormalEventReason = "NodePSHung"
+	NodeGPUXIPError   AbnormalEventReason = "NodeGPUXIPError"
+	NodeFDPressure    AbnormalEventReason = "NodeFDPressure"
+	NodePLEGUnhealthy AbnormalEventReason = "NodePLEGUnhealthy"
+	NodeNPTDown       AbnormalEventReason = "NodeNPTDown"
+	NodeNotReady      AbnormalEventReason = "NodeNotReady"
+	ConnTrackFull     AbnormalEventReason = "ConnTrackFull"
+
+	// Core Component Event
+	CcmSLBSyncFail          AbnormalEventReason = "CcmSLBSyncFail"
+	CcmSLBUnavailable       AbnormalEventReason = "CcmSLBUnavailable"
+	CcmSLBDeleteFail        AbnormalEventReason = "CcmSLBDeleteFail"
+	CcmCreateRouteFail      AbnormalEventReason = "CcmCreateRouteFail"
+	CcmSyncRouteFail        AbnormalEventReason = "CcmSyncRouteFail"
+	CcmAddNodeFail          AbnormalEventReason = "CcmAddNodeFail"
+	CcmDeleteNodeFail       AbnormalEventReason = "CcmDeleteNodeFail"
+	CcmSLBAnnotationChanged AbnormalEventReason = "CcmSLBAnnotationChanged"
+	CcmSLBSpecChanged       AbnormalEventReason = "CcmSLBSpecChanged"
+	CSISlowIO               AbnormalEventReason = "CSISlowIO"
+	CSIDeviceBusy           AbnormalEventReason = "CSIDeviceBusy"
+	CSIIOHang               AbnormalEventReason = "CSIIOHang"
+	CNIAllocIPFail          AbnormalEventReason = "CNIAllocIPFail"
+	CNIAllocResourceFail    AbnormalEventReason = "CNIAllocResourceFail"
+	CNIResourceInvalid      AbnormalEventReason = "CNIResourceInvalid"
+	CNIParseFail            AbnormalEventReason = "CNIParseFail"
+	CNIDisposeResourceFail  AbnormalEventReason = "CNIDisposeResourceFail"
+	ClusterIPNotEnough      AbnormalEventReason = "ClusterIPNotEnough"
+)
+
+type JudgeEventFunc func(event *v1.Event) bool
+
+type JudgeEvent struct {
+	kind  AbnormalEventReason
+	judge JudgeEventFunc
+}
+
+var (
+	normalEventCounter  *prometheus.CounterVec
+	errorEventCounter   *prometheus.CounterVec
+	warningEventCounter *prometheus.CounterVec
+
+	reasonToEventKind = map[string]AbnormalEventReason{
+		// Namespace level
+		"Evicted":            PodEvict,
+		"OOMKilling":         PodOOM,
+		"PodOOMKilling":      PodOOM,
+		"FailedMount":        VolumeFailMount,
+		"FailedAttachVolume": VolumeFailMount,
+
+		// Node level
+		"SystemOOM":             NodeOOM,
+		"Rebooted":              NodeRebooted,
+		"NodeHasDiskPressure":   NodeDiskPressure,
+		"DockerHung":            NodeDockerHung,
+		"PSProcessIsHung":       NodePSHung,
+		"NodeHasNvidiaXidError": NodeGPUXIPError,
+		"NodeHasFDPressure":     NodeFDPressure,
+		"PIDPressure":           NodeFDPressure,
+		"NodeHasPIDPressure":    NodeFDPressure,
+		"NTPIsDown":             NodeNPTDown,
+		"ConntrackFull":         ConnTrackFull,
+
+		// Core Component
+		"SyncLoadBalancerFailed":   CcmSLBSyncFail,
+		"DeleteLoadBalancerFailed": CcmSLBDeleteFail,
+		"CreateRouteFailed":        CcmCreateRouteFail,
+		"SYncRouteFailed":          CcmSyncRouteFail,
+		"AddNodeFailed":            CcmAddNodeFail,
+		"DeteleNodeFailed":         CcmDeleteNodeFail,
+		"UnAvailableLoadBalancer":  CcmSLBUnavailable,
+		"AnnotationChanged":        CcmSLBAnnotationChanged,
+		"ServiceSpecChanged":       CcmSLBSpecChanged,
+		"SlowIO":                   CSISlowIO,
+		"DeviceBusy":               CSIDeviceBusy,
+		"IOHang":                   CSIIOHang,
+		"AllocIPFailed":            CNIAllocIPFail,
+		"ParseFailed":              CNIParseFail,
+		"DisposeResourceFailed":    CNIDisposeResourceFail,
+	}
+
+	reasonToEventKindFunc = map[string][]JudgeEvent{
+		"Failed": {
+			{kind: PodFailStart, judge: isPodFailStart},
+			{kind: PodImagePullBackOff, judge: isPodImagePullBackOff},
+		},
+		"BackOff": {
+			{kind: PodCrash, judge: isPodCrash},
+			{kind: PodImagePullBackOff, judge: isPodImagePullBackOff},
+		},
+		"FailedCreate": {{kind: FailCreatePodExceedQuota, judge: isFailCreatePodExceedQuota}},
+		"FailedCreatePodSandBox": {
+			{kind: FailCreateContainerDiskNotEnough, judge: isFailCreateContainerDiskNotEnough},
+		},
+		"FailedScheduling": {
+			{kind: ResourceInsufficient, judge: isResourceInsufficient},
+			{kind: PodFailScheduling, judge: always},
+		},
+		"ProvisioningFailed": {
+			{kind: DiskProvisionFailSize, judge: isDiskProvisionFailSize},
+			{kind: DiskProvisionFail, judge: isDiskProvisionFail},
+		},
+		"FailedBinding": {
+			{kind: FailedBindingNoStorageClass, judge: isFailedBindingNoStorageClass},
+		},
+		"NodeNotReady": {
+			{kind: NodePLEGUnhealthy, judge: isNodePLEGUnhealthy},
+			{kind: NodeNotReady, judge: isNodeNotReady},
+		},
+		"AllocResourceFailed": {
+			{kind: ClusterIPNotEnough, judge: isClusterIPNotEnough},
+			{kind: CNIAllocResourceFail, judge: always},
+		},
+		"ResourceInvalid": {
+			{kind: ClusterIPNotEnough, judge: isClusterIPNotEnough},
+			{kind: CNIResourceInvalid, judge: always},
+		},
+	}
+)
+
+func init() {
+	normalEventCounter = prometheus.NewCounterVec(
+		prometheus.CounterOpts{
+			Namespace: "eventer",
+			Subsystem: "events",
+			Name:      "normal_total",
+		},
+		[]string{"reason", "namespace", "kind"},
+	)
+	errorEventLabels := []string{
+		"reason",
+		"kind",
+		"name",
+		"namespace",
+	}
+	errorEventCounter = prometheus.NewCounterVec(prometheus.CounterOpts{
+		Namespace: "eventer",
+		Subsystem: "events",
+		Name:      "error_total",
+	}, errorEventLabels)
+	warningEventCounter = prometheus.NewCounterVec(prometheus.CounterOpts{
+		Namespace: "eventer",
+		Subsystem: "events",
+		Name:      "warning_total",
+	}, errorEventLabels)
+
+	prometheus.MustRegister(normalEventCounter)
+	prometheus.MustRegister(warningEventCounter)
+	prometheus.MustRegister(errorEventCounter)
+}
+
+func event2Labels(kind AbnormalEventReason, event *v1.Event) []string {
+	return []string{
+		string(kind),
+		event.InvolvedObject.Kind,
+		event.InvolvedObject.Name,
+		event.InvolvedObject.Namespace,
+	}
+}
+
+func eventCounterInc(reason, namespace, kind string) {
+	normalEventCounter.WithLabelValues(reason, namespace, kind).Inc()
+}
+
+func cleanErrorEvent(reason AbnormalEventReason, event *v1.Event) {
+	labels := event2Labels(reason, event)
+	errorEventCounter.DeleteLabelValues(labels...)
+}
+
+func recordErrorEvent(reason AbnormalEventReason, event *v1.Event) {
+	labels := event2Labels(reason, event)
+	errorEventCounter.WithLabelValues(labels...).Inc()
+}
+
+func recordWarningEvent(reason AbnormalEventReason, event *v1.Event) {
+	labels := event2Labels(reason, event)
+	warningEventCounter.WithLabelValues(labels...).Inc()
+}
+
+func cleanWarningEvent(reason AbnormalEventReason, event *v1.Event) {
+	labels := event2Labels(reason, event)
+	warningEventCounter.DeleteLabelValues(labels...)
+}
+
+func triageEvent(event *v1.Event) (AbnormalEventReason, bool) {
+	if kind, ok := reasonToEventKind[event.Reason]; ok {
+		return kind, true
+	}
+	if judgements, ok := reasonToEventKindFunc[event.Reason]; ok {
+		for _, j := range judgements {
+			if j.judge(event) {
+				return j.kind, true
+			}
+		}
+	}
+	return "", false
+}
+
+// RecordEvent records event to prometheus metrics
+func RecordEvent(event *v1.Event) {
+	if kind, ok := triageEvent(event); ok {
+		recordErrorEvent(kind, event)
+	} else {
+		if event.Type == v1.EventTypeWarning {
+			recordWarningEvent(AbnormalEventReason(event.Reason), event)
+		} else {
+			eventCounterInc(event.Reason, event.Namespace, event.InvolvedObject.Kind)
+		}
+	}
+}
+
+// CleanEvent cleans event from prometheus metrics
+func CleanEvent(event *v1.Event) {
+	if kind, ok := triageEvent(event); ok {
+		cleanErrorEvent(kind, event)
+	} else if event.Type == v1.EventTypeWarning {
+		cleanWarningEvent(AbnormalEventReason(event.Reason), event)
+	}
+
+}
